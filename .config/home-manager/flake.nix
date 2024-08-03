d6952674593b1c851cd372ce06a13b6176a22e40
Jesse Bellingham <5078290+jbellingham@users.noreply.github.com>
2024-08-03 18:53:46 +1000

    Example packages overlay

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -11,16 +11,37 @@
       flake = false;
     };
   };
-  outputs = { nixpkgs, home-manager, nixGL, ... }:
+  outputs = {
+    nixpkgs,
+    # nixpkgs_2311,
+    home-manager,
+    nixGL,
+    ...
+  }:
   let
     system = "x86_64-linux";
+    # oldPkgsSrc = builtins.fetchTarball {
+    #   # https://github.com/nixos/nixpkgs/tree/nixos-23.11
+    #   url = "https://github.com/nixos/nixpkgs/archive/219951b495fc2eac67b1456824cc1ec1fd2ee659.tar.gz";
+    #   sha256 = "sha256-u1dfs0ASQIEr1icTVrsKwg2xToIpn7ZXxW3RHfHxshg=";
+    #   name = "source";
+    # };
+
+    # oldPkgs = import oldPkgsSrc {
+    #   inherit system;
+    #   config.allowUnfree = true;
+    # };
+
     pkgs = import nixpkgs {
       inherit system;
       config.allowUnfree = true;
+      # overlays = [ (final: prev: { glibc = oldPkgs.glibc; }) ];
     };
+
     nixgl = import nixGL {
       inherit pkgs;
     };
+
   in {
     homeConfigurations."jesse" = home-manager.lib.homeManagerConfiguration {
       inherit pkgs;
